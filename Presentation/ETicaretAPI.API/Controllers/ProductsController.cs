a282b3b3b4aa0090bd5bd36525997180be2eea9c
atasalim <atasalim@4NAR020>
2024-06-06 16:50:35 +0400

    Add project files.

---
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Persistence.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductWriteRepository _productWriteRepository;
+        private readonly IProductReadRepository _productReadRepository;
+
+        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+        }
+
+
+        //[HttpGet]
+        //public IActionResult Get()
+        //{
+        //    //var products = _productService.GetProducts();
+        //    //return Ok(products);
+        //}
+    }
+}
